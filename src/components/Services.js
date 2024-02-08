3562ffc0690b12961dd310315054e37113fea304
ANTOINE1128 <makdessyantoine@gmail.com>
2024-02-08 15:01:07 +0200

    services page styling

--- src/components/Services.js
+++ src/components/Services.js
@@ -23,9 +23,9 @@ const services =[
 ];
 const Services = () => {
   return ( 
-  <section className='section' id='services'>
+  <section className='section mt-20' id='services'>
    <div className='container mx-auto'>
-    <div className='flex flex-col lg:flex-row'>
+    <div className='flex flex-col lg:flex-row '>
 
       <div className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0'>
         <h2 className='h2 text-accent mb-6'>What I Do.</h2>
@@ -33,31 +33,33 @@ const Services = () => {
         </h3>
         <button className='btn btn-sm'>see my work</button>
         {/* #services */}
-      </div>
+  
         {/* #services list */}
-       <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
-        {services.map((service, index) =>{
-          const { name,description,link} = service;
-          return (
-          <div className='border-b border-white/20 h-[140px] mb-[38px] flex' key={index}>
-            <div className='max-w-[476px]'>
-              <h4 className='text-[20px] tracking-wider font-primary font-semi'>{name}</h4>
-              <p>{description}</p>
-            </div>
-            <div className='flex flex-col flex-1 items-end'>
-              <a href='#' className='btn w-9 h-9 mb-[42px] flex justify-center items-center'>
-                <BsArrowBarRight />
-              </a>
-              <a href='#' className='text-gradient text-sm '>{link}</a>
-            </div>
-          </div>
-          );
-        })}
+
+   <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0 flex-wrap justify-between'>
+   {services.map((service, index) =>{
+    const { name,description,link} = service;
+    return (
+    <div className='border-b border-white/20 h-[140px] mb-[38px] flex' key={index}>
+      <div className='max-w-[476px]'>
+        <h4 className='text-[20px] tracking-wider font-primary font-semi'>{name}</h4>
+        <p>{description}</p>
+      </div>
+      <div className='flex flex-col flex-1 items-end'>
+        <a href='#' className='btn w-9 h-9 mb-[42px] flex justify-center items-center'>
+          <BsArrowBarRight />
+        </a>
+        <a href='#' className='text-gradient text-sm '>{link}</a>
+      </div>
+    </div>
+    );
+  })}
        </div>
       </div>
-    </div>;
+    </div>
+  </div>
   </section>
-  )
+  );
 };
 
 export default Services;
